91dbf88df6342384a95fca3cf79a3745dd4c77df
System Administrator <root@192.168.1.130>
2018-07-18 23:42:17 +0200

    first commit

---
+++ src/pages/Settings.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="welcome-class">
+    <Header></Header> 
+    <h2>{{mainText}}</h2>
+  </div>
+</template>
+
+<script>
+// Set the parent router view.
+// The parent route component is Home.vue in pages.
+// Loads home.vue from here.
+import Header from './components/Header.vue'
+  export default {
+    name: 'Settings',
+    data: function(){
+      return {
+        mainText: 'Settings'
+      }
+    },
+    components: {Header}
+  }
+</script>
+
+<style scoped lang="scss">
+  .welcome-class {
+    padding: 5rem;
+    display: block;
+    h2 {
+      margin-top: 0;
+      margin-bottom: 2em;
+    }
+    padding: 0;
+    background: coral;
+    border-radius: 4px;
+    font-size: 24px;
+    text-align: center;
+    .btn-wrapper {
+     
+      a {
+        color: #fff;
+        text-decoration: none;
+        padding: 1rem;
+        border-radius: 3px;
+      }
+      .hi {
+        background: #6c6cd6;
+      }
+      .bi {
+        background: #d66c87
+      }
+    }
+  }
+</style>
---
+++ src/pages/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <ul class="navigation">
+            <li class="link"><a href="/">Home</a></li>
+            <li class="link"><a href="/settings">Settings</a></li>
+        </ul>
+       {{myData}}
+    </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data: function() {
+    return {
+      myData: ["Home", "Settings"]
+    };
+  }
+};
+</script>
+<style scoped lang="scss">
+.navigation {
+  text-align: left;
+ 
+  list-style-type: none;
+  -webkit-margin-before: 0em;
+  -webkit-margin-after: 0em;
+  -webkit-margin-start: 0px;
+  -webkit-margin-end: 0px;
+  -webkit-padding-start: 0px;
+   padding: 1rem;
+  li.link {
+    font-size: 1rem;
+    color: white;
+    list-style: none;
+    display: inline-block;
+    a {
+      color: white;
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+}
+</style>
