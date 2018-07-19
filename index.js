41d852be0f26c04876ba35287f618b8fa7b31b30
System Administrator <root@MacBook-Pro-de-Javier.local>
2018-07-19 15:21:00 +0200

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const MongoClient = require('mongodb').MongoClient
 
-var db 
-
-MongoClient.connect('mongodb://dbuser:all4pass@ds235711.mlab.com:35711/mymongodb', (err, client) => {
-  if (err) return console.log(err)
-  console.log('okmackey')
-})
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
